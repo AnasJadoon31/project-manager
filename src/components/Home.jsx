43955e9abeac0d6cec84bfa240c99a35016d16fe
MUHAMMAD ANAS <anasjadoon31@gmail.com>
2025-08-08 18:46:36 +0500

    logo fix

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,10 @@
-import AddProject from "./AddProject.jsx";
-
 export default function Home({setScreen}) {
     const createNewProject = () => {
         setScreen(1);
     };
     return (
         <div className="flex flex-col p-4 items-center margin-center-home">
-            <img className="size-20 content-center" src="/public/logo.png" alt="logo"/>
+            <img className="size-20 content-center" src="/logo.png" alt="logo"/>
             <h2 className="text-2xl font-bold text-yellow-900 mt-3">No Project Selected</h2>
             <p className="text-sm font-semibold text-stone-400 mt-5">Select a project or get started with a new one</p>
             <button className="mt-10 bg-stone-700 w-48 p-2 rounded-2xl
